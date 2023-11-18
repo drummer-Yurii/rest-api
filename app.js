a99d791e0d1575125c33d897df50832f8d766ee3
Yurii Rybachok <drummer2009@ukr.net>
2023-11-18 11:53:33 +0200

    add connection to mongoDB

--- app.js
+++ app.js
@@ -3,3 +3,23 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 const app = express();
+
+//Database
+mongoose.connect('mongodb://localhost/motivation');
+
+const db = mongoose.connection;
+
+db.once('open', () => {
+    console.log('Connecting to MongoDB database...');
+})
+
+// Middleware
+app.use(bodyParser.json());
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// Starting server
+app.listen(3000, console.log('Listening on port 3000'));
